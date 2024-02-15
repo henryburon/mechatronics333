316885479903f7704b61efaa3c64597b24f5eb70
Henry Buron <henryburon34@gmail.com>
2024-02-15 16:49:06 -0600

    Blinks every 0.5s by switching between 25% and 75% duty cycle

--- feedback_control_pwm/pwm.c
+++ feedback_control_pwm/pwm.c
@@ -4,12 +4,12 @@ int main(void) {
 
    NU32DIP_Startup();          // cache on, interrupts on, LED/button init, UART init
    
-   T3CONbits.TCKPS = 0b000;  // set prescaler (N) on Timer3 to N=1 (1:1)
+   T3CONbits.TCKPS = 0b000;  // set prescaler (N) on Timer3 to N=1 (1:1). Prescaler divides the incoming clock before incrementing the timer
    PR3 = 2399;               // period = (PR3 + 1) * N * (1/48,000,000) = 0.0005s, 50 us, 20 kHz
    TMR3 = 0;                 // initial timer3 count is 0
    OC1CONbits.OCM = 0b110;   // PWM mode on OC1, fault pin disabled
    OC1CONbits.OCTSEL = 1;   // timer3 is the clock source for this Output Compare module
-   OC1RS = 600;             // duty cycle = OC1RS/(PR3 + 1) = 25%
+   OC1RS = 600;             // duty cycle = OC1RS/(PR3 + 1) = 25%. Initializes duty cycle (and LED brightness) at 25% of max.
    OC1R = 600;              // initialize before turning OC1 on; afterward it is read only
 
    T3CONbits.ON = 1;        // turn on Timer3
@@ -19,29 +19,23 @@ int main(void) {
 
    OC1CONbits.ON = 1;       // turn on OC1    
 
-
-
-  _CP0_SET_COUNT(0);       // delay 2 seconds to see the 25% duty cycle on a 'scope
-  while(_CP0_GET_COUNT() < 2 * 40000000) {
-    ;
-  }
-  OC1RS = 1800;            // set duty cycle to 50%
-
-  _CP0_SET_COUNT(0);       // delay 2 seconds to see the 25% duty cycle on a 'scope
-  while(_CP0_GET_COUNT() < 2 * 40000000) {
-    ;
-  }
-  OC1RS = 600;            // set duty cycle to 50%
-
-  _CP0_SET_COUNT(0);       // delay 2 seconds to see the 25% duty cycle on a 'scope
-  while(_CP0_GET_COUNT() < 2 * 40000000) {
-    ;
-  }
-  OC1RS = 1800;            // set duty cycle to 50%
-  
   while(1) {
-    ;                      // infinite loop
+
+  _CP0_SET_COUNT(0);
+  while(_CP0_GET_COUNT() < 0.5 * 40000000) {  // delay 0.5 s
+    ;
   }
+  OC1RS = 1800; // set duty cycle to 75%
+
+  _CP0_SET_COUNT(0);
+  while(_CP0_GET_COUNT() < 0.5 * 40000000) {  // delay 0.5 s
+    ;
+  }
+  OC1RS = 600; // set duty cycle to 75%
+
+  }
+
+
   return 0;
 }
 
