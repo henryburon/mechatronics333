ba9ae329a7c32e6aff9fcec290b7dbfdd4afcd87
Henry Buron <henryburon34@gmail.com>
2024-02-15 16:51:44 -0600

    Operates at duty cycle 75%

--- feedback_control_pwm/pwm.c
+++ feedback_control_pwm/pwm.c
@@ -2,38 +2,37 @@
 
 int main(void) {
 
-   NU32DIP_Startup();          // cache on, interrupts on, LED/button init, UART init
-   
-   T3CONbits.TCKPS = 0b000;  // set prescaler (N) on Timer3 to N=1 (1:1). Prescaler divides the incoming clock before incrementing the timer
-   PR3 = 2399;               // period = (PR3 + 1) * N * (1/48,000,000) = 0.0005s, 50 us, 20 kHz
-   TMR3 = 0;                 // initial timer3 count is 0
-   OC1CONbits.OCM = 0b110;   // PWM mode on OC1, fault pin disabled
-   OC1CONbits.OCTSEL = 1;   // timer3 is the clock source for this Output Compare module
-   OC1RS = 600;             // duty cycle = OC1RS/(PR3 + 1) = 25%. Initializes duty cycle (and LED brightness) at 25% of max.
-   OC1R = 600;              // initialize before turning OC1 on; afterward it is read only
+  NU32DIP_Startup();          // cache on, interrupts on, LED/button init, UART init
+  
+  T3CONbits.TCKPS = 0b000;  // set prescaler (N) on Timer3 to N=1 (1:1). Prescaler divides the incoming clock before incrementing the timer
+  PR3 = 2399;               // period = (PR3 + 1) * N * (1/48,000,000) = 0.0005s, 50 us, 20 kHz
+  TMR3 = 0;                 // initial timer3 count is 0
+  OC1CONbits.OCM = 0b110;   // PWM mode on OC1, fault pin disabled
+  OC1CONbits.OCTSEL = 1;   // timer3 is the clock source for this Output Compare module
+  OC1RS = 600;             // duty cycle = OC1RS/(PR3 + 1) = 25%. Initializes duty cycle (and LED brightness) at 25% of max.
+  OC1R = 600;              // initialize before turning OC1 on; afterward it is read only
 
-   T3CONbits.ON = 1;        // turn on Timer3
+  T3CONbits.ON = 1;        // turn on Timer3
 
-   // Need to map OC1 to a pin
-   RPB15Rbits.RPB15R = 0b0101;  // map OC1 to RPB15, pin 26 on board. 0b0101 designates the mapping as OC1
+  // Need to map OC1 to a pin
+  RPB15Rbits.RPB15R = 0b0101;  // map OC1 to RPB15, pin 26 on board. 0b0101 designates the mapping as OC1
 
-   OC1CONbits.ON = 1;       // turn on OC1    
+  OC1CONbits.ON = 1;       // turn on OC1
 
+  // delay 2 seconds
+  _CP0_SET_COUNT(0);
+  while(_CP0_GET_COUNT() < 2 * 40000000) {  // delay 2 s
+    ;
+  }
+
+  // set duty cycle to 75%
+  OC1RS = 1800;
+
+  // continue forever
   while(1) {
-
-  _CP0_SET_COUNT(0);
-  while(_CP0_GET_COUNT() < 0.5 * 40000000) {  // delay 0.5 s
     ;
   }
-  OC1RS = 1800; // set duty cycle to 75%
 
-  _CP0_SET_COUNT(0);
-  while(_CP0_GET_COUNT() < 0.5 * 40000000) {  // delay 0.5 s
-    ;
-  }
-  OC1RS = 600; // set duty cycle to 75%
-
-  }
 
 
   return 0;
